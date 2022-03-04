b6b983b43f64eb95317e589646f99fb918d01292
Karan Gupta <56931893+Karan381@users.noreply.github.com>
2022-03-04 15:44:49 +0530

    ui updated

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,34 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
-    
-    [SerializeField]int currentBalance;
-
+    [SerializeField] int currentBalance;
     [SerializeField] float timeDelay = 1f;
 
+    [SerializeField] TextMeshProUGUI displayBalance;
     private void Awake()
     {
         currentBalance = startingBalance;
+        updateDisplay();
     }
     public int CurrentBalance { get { return currentBalance; } }
 
     public void Deposit(int Amount)
     {
         currentBalance += Mathf.Abs(Amount);
+        updateDisplay();
     }
 
     public void Withdraw(int Amount)
     {
         currentBalance -= Mathf.Abs(Amount);
-
+        updateDisplay();
         if(currentBalance < 0)
         {
             Invoke("ReloadScene", timeDelay);
-            
+            //GameLost
         }
     }
     // Start is called before the first frame update
@@ -37,5 +39,10 @@ public class Bank : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    void updateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 }
